b74e4e4778554201b67c825fd3bf4f53ca09592f
1324589321@qq.com <1324589321@qq.com>
2022-01-20 20:51:30 +0800

    添加sass

--- components/title.js
+++ components/title.js
@@ -1,11 +1,15 @@
 import useI18n from '../hooks/use-i18n'
+// import '../components/title.module.scss';
+import '../styles/title.module.scss'
+import styles from '../styles/title.module.scss'
 
 export default function Title({ username }) {
   // 语言切换呢
   const i18n = useI18n()
   return (
     <div>
-      <h1>test标题</h1>
+      <h1 className={styles.nav}>test标题111</h1>
+      <h2 className='test'>uuu</h2>
       <h1>{i18n.t('intro.welcome', { username })}</h1>
     </div>
   )
